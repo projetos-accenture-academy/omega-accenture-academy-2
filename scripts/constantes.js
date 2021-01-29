32699909a35d74bee812edf9da1aadb371a606b6
kellisonjk <kellisonjk@gmail.com>
2021-01-29 19:50:22 -0300

    Script com constates em comum em todas as páginas

---
+++ scripts/constantes.js
@@ -0,0 +1,15 @@
+/**
+ * IDs a serem utilizados para obter/salvar itens no localStorage
+ * Exemplo: 
+ *  - Obter os dados de login salvos no browser
+ *      localStorage.getItem(userDataCollection)
+ */
+
+// Dados obtidos no login
+const userDataCollection   = "@userDataCollection";
+
+// Planos de contas do usuário
+const userAccountPlans     = "@userAccountPlans";
+
+// Dados do dashboard (extrato de transações)
+const userAccountStatements = "@userAccountStatements";
