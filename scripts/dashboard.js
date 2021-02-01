5f7b6708458aafba9c82d0d9d7b81e8466c8262a
kellisonjk <kellisonjk@gmail.com>
2021-01-31 23:02:28 -0300

    Dashboard - aplicação do filtro no resultado da chamada de api

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -4,32 +4,36 @@ const viewContas = null || document.getElementById('view-contas')
  * Para testar sem o login, descomentar o bloco abaixo
  * Talvez seja necessário trocar o token por um atualizado
  */
-/*const usuarioDados = `{
+const usuarioDados = `{
   "usuario": {
     "id": 8,
     "nome": "Usuário teste",
     "login": "usuarioteste"
   },
-  "token": "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ1c3VhcmlvdGVzdGUiLCJpZFVzdWFyaW8iOjgsImF1dGhvcml0aWVzIjpbIlJPTEVfVVNFUiJdLCJpYXQiOjE2MTIwMTg5MjUsImV4cCI6MTYxMjAyMjUyNX0.64tdLN187ik15MKe1Q4LIoqpZr1X0M8w9kUdf5WHIgje28VmFA82JgyBS57BBjbeLekwtIk3dQ_pmjs-fZof9A"
+  "token": "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ1c3VhcmlvdGVzdGUiLCJpZFVzdWFyaW8iOjgsImF1dGhvcml0aWVzIjpbIlJPTEVfVVNFUiJdLCJpYXQiOjE2MTIxNDM1NzgsImV4cCI6MTYxMjE0NzE3OH0.nY7hz8ivJVP7wHwv0BPUDTzdD8tAulgHuTdyaqcvDLqbESUrH56RjdfXvxMB_lyt9kDry-I2K54VYS5EQHF0-w"
 }`
 
-localStorage.setItem(userDataCollection, usuarioDados)*/
+localStorage.setItem(userDataCollection, usuarioDados)
 
 
 /**
  * Realiza a chamada da api de lanãmentos e salva no localstorage
  * Exibe um erro se não conseguir acessar a API 
  */
-const setDashboardData = async () => {
+const setDashboardData = async (initialDate = '', finalDate = '') => {
     
     const userData = JSON.parse(localStorage.getItem(userDataCollection));
     const {usuario, token} = await userData;
     let error = false;
-
-   // document.getElementById('view-contas').innerHTML = loadingElement;
+    const now = new Date();
+    const thisMonth = `${now.getFullYear()}-${(now.getMonth() <= 9 ? '0' + (now.getMonth()+1) : now.getMonth()+1)}`;
+    const lastDay = new Date(now.getFullYear(), now.getMonth(), 0).getDate()
+    
+    const ini = initialDate ? initialDate : `${thisMonth}-01`;
+    const final = finalDate ? finalDate : `${thisMonth}-${lastDay}`;
     
     await axios
-        .get(`${baseURL}dashboard?fim=2021-01-31&inicio=2021-01-01&login=${usuario.login}`,  {
+        .get(`${baseURL}dashboard?fim=${final}&inicio=${ini}&login=${usuario.login}`,  {
             headers: {
                 "Content-Type": "application/json",
                 "Authorization": token
@@ -73,7 +77,10 @@ const viewAccountItem = (conta, label) => {
                 <div class="accordion-body accordion-conta">
                     ${conta.lancamentos.length > 0 ?
                         `<div class="container mb-2 p-2">
-                            <div class="container p-0 mt-2 d-grid gap-2 d-md-flex justify-content-md-end">
+                            <div class="container p-0 mt-2 d-grid gap-2 d-md-flex justify-content-md-between">
+                                <div class="d-flex align-items-center">
+                                    <h6 id="filter-label">Extrato do mês atual</h6>
+                                </div>
                                 <button class="btn btn-primary btn-sm" type="button" data-bs-toggle="modal"
                                     data-bs-target="#modal-filter">
                                     Filtros <i class="fas fa-filter"></i>
@@ -106,14 +113,43 @@ const viewAccountItem = (conta, label) => {
 
 const init = async () => {
     viewContas.innerHTML = loadingElement;
-
+    
     const error = await setDashboardData()
 
     if(!error){
         const accounts = JSON.parse(localStorage.getItem(userAccountStatements))
         const {contaBanco: contaBancoDash, contaCredito: contaCreditoDash} = accounts
 
+        if(document.getElementById('apply-filter')){
+            document.getElementById('apply-filter').addEventListener('click', async function(e){
+                let iniDate = document.getElementById('initialDate').value
+                let finDate = document.getElementById('finalDate').value
+                if(iniDate != '' && finDate != ''){
+                    await setDashboardData(iniDate, finDate)
+                    const accountsRefreshed = JSON.parse(localStorage.getItem(userAccountStatements))
+                    const {contaBanco: contaBancoDash, contaCredito: contaCreditoDash} = accountsRefreshed
+                   
+                    viewContas.innerHTML =  viewAccountItem(contaBancoDash, 'Conta Banco') + viewAccountItem(contaCreditoDash, 'Conta Corrent')
+                    
+                    if(document.getElementById('filter-label')){
+                        const filterLabel = document.getElementById('filter-label')
+                        
+                        filterLabel.innerHTML = `${iniDate} à ${finDate}`
+                    }
+                   
+                } else {
+                    alert("Preencha os campos de datas (inicial e final)");
+                }
+            })
+        }
+
         viewContas.innerHTML =  viewAccountItem(contaBancoDash, 'Conta Banco') + viewAccountItem(contaCreditoDash, 'Conta Corrent')
+
+
+        const userData = JSON.parse(localStorage.getItem(userDataCollection));
+        const {usuario} =  userData;
+        
+        document.getElementById('user-logged').innerHTML = usuario.nome.split(" ")[0]
     } else {
         viewContas.innerHTML =  `<di class="error-dash">Não foi possível obter os dados do dashboard, por favor tente novamente.</div>`;
     }
